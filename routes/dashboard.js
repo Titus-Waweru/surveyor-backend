8a9419d8280486892ee381526f77ccf45c6f5785
Titus Waweru <tituswaweru631@gmail.com>
2025-05-25 21:00:49 +0300

    Switch from bcrypt to bcryptjs to fix Linux deployment error

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,7 +4,7 @@ const { PrismaClient } = require("@prisma/client");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient();
 
